ff194f0bf6e49301dc712cd89c45f0df62350d9b
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-02-16 20:22:05 +0700

    add controller stores and active index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'stores/index'
+  root "stores#index"
+  
   resources :products
   
-  root "products#index"
+  # root "products#index"
+
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
