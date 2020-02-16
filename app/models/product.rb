8f00d0999b08f529c4a56eaa67202601fdcbcaa5
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-02-16 18:53:48 +0700

    add validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 1.0}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => { :with => %r{\.(gif|jpg|png)\Z}i, :message => 'Just received file GIF, JPG, PNG'}
 end
