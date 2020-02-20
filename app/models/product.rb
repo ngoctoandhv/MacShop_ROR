f07f4fe5514789af38d12f88e7b9ae065d10f3d0
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-02-20 19:42:24 +0700

    add session cart and add quanhe giua cac model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,21 @@ class Product < ApplicationRecord
   validates :price, :numericality => {:greater_than_or_equal_to => 1.0}
   validates :title, :uniqueness => true
   validates :image_url, :format => { :with => %r{\.(gif|jpg|png)\Z}i, :message => 'Just received file GIF, JPG, PNG'}
+
+  has_many :line_items
+  before_destroy :check_if_has_line_item
+
+  private
+
+    def check_if_has_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'This product has a LineItem')
+            return false
+        end
+    end
+    #Phương thức này sẽ kiểm tra xem một đối tượng Product có liên kết nào tới một đối tượng LineItem nào không, 
+    #nếu không thì trả về true, ngược lại thì chúng ta tạo một phần tử trong thuộc tính errors có khóa là :base 
+    #và giá trị là 'This product has a LineItem', rồi trả về false.
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_120747) do
+ActiveRecord::Schema.define(version: 2020_02_20_123647) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
