e45dd29a639d4a31c82817d0bf85e3692bafef27
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-02-20 20:11:15 +0700

    add button cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,8 +24,10 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
-
+    # @line_item = LineItem.new(line_item_params)
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(:product => product)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
