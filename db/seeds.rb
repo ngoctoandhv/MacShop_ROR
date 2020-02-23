b4fbf2393104165e319a845d44fe6c0139c8fa05
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-02-23 10:03:42 +0700

    render and custom cart to sidebar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
-Product.create(:title => 'Learn Web Development with Rails',
+Product.create(:title => 'Macbook Air 13 128GB MQD32SA/A (2017)',
     :description => 
     %{
-        Ruby on Rails Tutorial book and screencast series 
-        teach you how to develop and deploy real, 
-        industrial-strength web applications with Ruby on Rails.
+      Macbook Air 13 128 GB MQD32SA/A (2017) với thiết kế không thay đổi, vỏ nhôm sang trọng, siêu mỏng và siêu nhẹ, hiệu năng được nâng cấp, thời lượng pin cực lâu,.
     },
     :image_url => 'mac.jpg',
-    :price => 29.99)
+    :price => 1000.00)
   
-Product.create(:title => 'The Ruby Programming Language',
+Product.create(:title => 'Macbook 12 512GB (2017)',
     :description =>
     %{
-        The Ruby Programming Language is the authoritative guide 
-        to Ruby and provides comprehensive coverage 
-        of versions 1.8 and 1.9 of the language.
+      MacBook 12 2017 với đường nét thiết kế không có thay đổi so với phiên bản 2016 nhưng Apple đã nâng cấp thêm bộ nhớ và giới thiệu bộ vi xử lý Intel thế hệ thứ 7 (Kaby Lake).
     },
     :image_url => 'mac.jpg',
-    :price => 39.99)
+    :price => 1200.00)
+
+Product.create(:title => 'Macbook Air 13 128GB 2019',
+    :description =>
+    %{
+      MacBook Air 128GB 2019 có thiết kế tuyệt đẹp, màn hình Retina Truetone đẳng cấp và trải nghiệm bàn phím tuyệt vời nhất từ trước đến nay..
+    },
+    :image_url => 'mac.jpg',
+    :price => 1300.00)
